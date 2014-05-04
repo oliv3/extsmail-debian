4e520b07d67ef5871c908fc1e80956bc48fc24bb
Olivier Girondel <olivier@biniou.info>
2014-05-04 18:45:02 +0200

    Trash the build directory.

--- make_deb.sh
+++ make_deb.sh
@@ -4,7 +4,8 @@ PKG=extsmail-$VSN
 TAR=$PKG.tar.gz
 DESTDIR=build-debian
 
-mkdir -p $DESTDIR
+rm -rf $DESTDIR
+mkdir $DESTDIR
 ln -sf ../$TAR $DESTDIR/extsmail_$VSN.orig.tar.gz
 cd $DESTDIR && tar xvf ../$TAR && cd $PKG && cp -r ../../debian . && cd ../.. 
 cd $DESTDIR/$PKG && pdebuild --auto-debsign -- --twice && cd ../..
