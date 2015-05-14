2d26f017a0b4e25ac84c9da78911f9dcedbc4a5c
Olivier Girondel <olivier@biniou.info>
2015-05-15 00:55:08 +0200

    Build for unstable.

--- make_deb.sh
+++ make_deb.sh
@@ -3,9 +3,10 @@ VSN=2.0
 PKG=extsmail-$VSN
 TAR=$PKG.tar.gz
 DESTDIR=build-debian
+dist=unstable
 
 rm -rf $DESTDIR
 mkdir $DESTDIR
 ln -sf ../$TAR $DESTDIR/extsmail_$VSN.orig.tar.gz
 cd $DESTDIR && tar xvf ../$TAR && cd $PKG && cp -r ../../debian . && cd ../.. 
-cd $DESTDIR/$PKG && pdebuild --auto-debsign -- --twice && cd ../..
+cd $DESTDIR/$PKG && pdebuild --auto-debsign -- --buildresult /var/cache/pbuilder/result-$dist --distribution $dist --basetgz /var/cache/pbuilder/base-$dist.tgz --twice && cd ../..
