7b212cfb1bd57d3d91a2e1cf8b07796803d72565
Olivier Girondel <olivier@biniou.info>
2018-11-13 16:09:38 +0100

    Update build script

--- make_deb.sh
+++ make_deb.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
-VSN=2.0
+#!/usr/bin/env bash
+VSN=2.1
 PKG=extsmail-$VSN
 TAR=$PKG.tar.gz
 DESTDIR=build-debian
-dist=unstable
 
 rm -rf $DESTDIR
 mkdir $DESTDIR
 ln -sf ../$TAR $DESTDIR/extsmail_$VSN.orig.tar.gz
 cd $DESTDIR && tar xvf ../$TAR && cd $PKG && cp -r ../../debian . && cd ../.. 
-cd $DESTDIR/$PKG && pdebuild --auto-debsign -- --buildresult /var/cache/pbuilder/result-$dist --distribution $dist --basetgz /var/cache/pbuilder/base-$dist.tgz --twice && cd ../..
+cd $DESTDIR/$PKG && pdebuild --auto-debsign -- --twice && cd ../..
